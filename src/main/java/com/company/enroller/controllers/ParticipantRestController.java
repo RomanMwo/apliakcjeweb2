28cc309869ce8b98d1706809c222bf6b1d3424f0
student16 <student16@pcoip.ki.agh.edu.pl>
2018-05-13 14:38:25 +0200

    second

---
+++ src/main/java/com/company/enroller/controllers/ParticipantRestController.java
@@ -0,0 +1,54 @@
+package com.company.enroller.controllers;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.company.enroller.model.Participant;
+import com.company.enroller.persistence.ParticipantService;
+
+@RestController
+@RequestMapping("/participants")
+public class ParticipantRestController {
+
+	@Autowired
+	ParticipantService participantService;
+
+	@RequestMapping(value = "", method = RequestMethod.GET)
+	public ResponseEntity<?> getParticipants() {
+		Collection<Participant> participants = participantService.getAll();
+		return new ResponseEntity<Collection<Participant>>(participants, HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> getMeeting(@PathVariable("id") String login) {
+	     Participant participant = participantService.findByLogin(login);
+	     if (participant == null) {
+	         return new ResponseEntity(HttpStatus.NOT_FOUND);
+	     }
+	     return new ResponseEntity<Participant>(participant, HttpStatus.OK);
+	 }
+		
+	 @RequestMapping(value = "", method = RequestMethod.POST)
+	 public ResponseEntity<?> addParticipant(@RequestBody Participant participant){
+		 if (participantService.findByLogin(participant.getLogin())!= null) {
+			 return new ResponseEntity("Participant with login " + participant.getLogin() + "arleady exist" , HttpStatus.CONFLICT);
+		 }
+		 participantService.addParticipant(participant);
+		 return new ResponseEntity(participant, HttpStatus.OK);
+	 }
+	 
+	 @RequestMapping(value = "", method = RequestMethod.DELETE)
+	 public ResponseEntity<?> remParticipant(@RequestBody Participant participant){
+		
+		 participantService.remParticipant(participant);
+		 return new ResponseEntity(participant, HttpStatus.OK);
+	 }
+}
